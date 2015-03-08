fb865a18e72942fa4340b899e2c70e2d785b7357
Max Bernstein <tekknolagi@gmail.com>
2015-03-08 18:07:18 -0400

    Another small restructure.

--- canvas.js
+++ canvas.js
@@ -1,6 +1,12 @@
 var FONT = "15pt Arial";
 
+CanvasRenderingContext2D.prototype.setDimensions = function (img) {
+    this.canvas.height = img.height;
+    this.canvas.width = img.width;
+};
+
 CanvasRenderingContext2D.prototype.caption = function (img, caption) {
+    this.setDimensions(img);
     this.drawImage(img, 0, 0);
     this.font = FONT;
     this.fillStyle = "white";
@@ -9,7 +15,7 @@ CanvasRenderingContext2D.prototype.caption = function (img, caption) {
     this.shadowOffsetX = 5;
     this.shadowOffsetY = 5;
     this.fillText(caption, 10, img.height - 10);
-}
+};
 
 CanvasRenderingContext2D.prototype.clear = function () {
     this.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -29,8 +35,6 @@ window.onload = function () {
     /* From robertnyman.com/2011/03/10/using-html5-canvas-drag-and-drop-and-file-api-to-offer-the-cure/. */
     img.addEventListener("load", function () {
 	ctx.clear();
-	canvas.height = img.height;
-	canvas.width = img.width;
 	ctx.caption(img, caption.value);
     }, false);
     
@@ -45,7 +49,8 @@ window.onload = function () {
 	var files = e.dataTransfer.files;
 	if (files.length > 0) {
 	    var file = files[0];
-	    if (typeof FileReader !== "undefined" && file.type.indexOf("image") != -1) {
+	    if (typeof FileReader !== "undefined" &&
+		file.type.indexOf("image") != -1) {
 		var reader = new FileReader();
 		// Note: addEventListener doesn't work in Google Chrome for this event
 		reader.onload = function (e) { img.src = e.target.result; };
